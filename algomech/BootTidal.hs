7be319036956b447b69a79e84037b0ded2ddb0b8
Oscar South <oscarsouth@avlinux8>
2019-04-18 21:41:58 +0100

    tidal 1.0.11

--- algomech/BootTidal.hs
+++ algomech/BootTidal.hs
@@ -8,32 +8,36 @@ import Sound.Tidal.Scales
 -- total latency = oLatency + cFrameTimespan
 tidal <- startTidal (superdirtTarget {oLatency = 0.2, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
 
-p = streamReplace tidal
-hush' = streamHush tidal
-mute = streamMute tidal
-unmute = streamUnmute tidal
-solo = streamSolo tidal
-unsolo = streamUnsolo tidal
-once = streamOnce tidal False
-asap = streamOnce tidal True
-nudgeAll = streamNudgeAll tidal
-setcps = asap . cps
-xfade = transition tidal (Sound.Tidal.Transition.xfadeIn 4)
-xfadeIn t = transition tidal (Sound.Tidal.Transition.xfadeIn t)
-histpan t = transition tidal (Sound.Tidal.Transition.histpan t)
-wait t = transition tidal (Sound.Tidal.Transition.wait t)
-waitT f t = transition tidal (Sound.Tidal.Transition.waitT f t)
-jump = transition tidal (Sound.Tidal.Transition.jump)
-jumpIn t = transition tidal (Sound.Tidal.Transition.jumpIn t)
-jumpIn' t = transition tidal (Sound.Tidal.Transition.jumpIn' t)
-jumpMod t = transition tidal (Sound.Tidal.Transition.jumpMod t)
-mortal lifespan release = transition tidal (Sound.Tidal.Transition.mortal lifespan release)
-interpolate = transition tidal (Sound.Tidal.Transition.interpolate)
-interpolateIn t = transition tidal (Sound.Tidal.Transition.interpolateIn t)
-clutch = transition tidal (Sound.Tidal.Transition.clutch)
-clutchIn t = transition tidal (Sound.Tidal.Transition.clutchIn t)
-anticipate = transition tidal (Sound.Tidal.Transition.anticipate)
-anticipateIn t = transition tidal (Sound.Tidal.Transition.anticipateIn t)
+let p = streamReplace tidal
+let hush = streamHush tidal
+let list = streamList tidal
+let mute = streamMute tidal
+let unmute = streamUnmute tidal
+let solo = streamSolo tidal
+let unsolo = streamUnsolo tidal
+let once = streamOnce tidal
+let asap = once
+let nudgeAll = streamNudgeAll tidal
+let all = streamAll tidal
+let resetCycles = streamResetCycles tidal
+let setcps = asap . cps
+let xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
+let xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
+let histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
+let wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
+let waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
+let jump i = transition tidal True (Sound.Tidal.Transition.jump) i
+let jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
+let jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
+let jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
+let mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
+let interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
+let interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
+let clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
+let clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
+let anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
+let anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
+let forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
 d01 = p "1"
 d02 = p "2"
 d03 = p "3"
@@ -102,7 +106,7 @@ up4 = within (0.825,1)
 :{
 adagio = do
     setCC 6 51 127
-    setcps (65/60/4)
+    setcps (71/60/4)
 :}
 :{
 andante = do
